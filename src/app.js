c63311a4ca5c79f4b6fceddf0ef889b61ddf0670
alireza139 <alireza.hosseinii66@gmail.com>
2025-11-27 12:04:09 +0330

    header development - part 4

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 const toggleThemeBtns = document.querySelectorAll(".toggle-theme");
-console.log(toggleThemeBtns);
-
+const submenuBtn = document.querySelector(".submenu-btn")
+const submenu = document.querySelector(".submenu")
+const bars3 = document.querySelector(".bars3")
+const offcanv = document.querySelector(".offcanv")
+const overlay = document.querySelector(".overlay")
+const offcanvCloseBtn = document.querySelector(".offcanv-close-btn")
 
 toggleThemeBtns.forEach(btn => {
     btn.addEventListener("click", function () {
@@ -12,4 +16,28 @@ toggleThemeBtns.forEach(btn => {
             localStorage.setItem("theme", "dark");
         }
     })
+})
+
+
+submenuBtn.addEventListener("click", (event) => {
+    event.target.parentElement.classList.toggle("text-orange-300");
+    submenu.classList.toggle("submenu-open")
+});
+
+bars3.addEventListener("click", () => {
+    offcanv.classList.remove("-right-64");
+    overlay.classList.remove("overlay-close");
+
+    offcanv.classList.add("right-0");
+    overlay.classList.add("overlay-open");
+});
+
+offcanvCloseBtn.addEventListener("click", (event) => {
+    console.log(event.target);
+
+    offcanv.classList.remove("right-0");
+    overlay.classList.remove("overlay-open");
+
+    offcanv.classList.add("-right-64");
+    overlay.classList.add("overlay-close");
 })
